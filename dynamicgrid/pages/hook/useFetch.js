b72c809fafaad61b571ed02c97a2e49c7ca32580
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-13 20:01:09 +0530

    useFetch transfer to hook directory

--- dynamicgrid/pages/hook/useFetch.js
+++ dynamicgrid/pages/hook/useFetch.js
@@ -3,10 +3,12 @@ function useFetch(url) {
   const [data, setData] = useState([]);
   const [isComplete, setCompleted] = useState(false);
   async function fetchUrl(Url) {
+    console.log("fetching will start");
     const response = await fetch(Url);
     const json = await response.json();
-    setData(json)
+    setData(json);
     setCompleted(true);
+    console.log("fetchling is over");
   }
   useEffect(() => {
     fetchUrl(url);
--- dynamicgrid/pages/index.js
+++ dynamicgrid/pages/index.js
@@ -1,11 +1,12 @@
 import Loading from "./Loading";
 import Grid from "./Grid";
-import { useFetch } from './api/useFetch';
+import { useFetch } from './hook/useFetch';
 import dataFormatter from "./util/dataFormatter";
 export default function Home() {
   const Url =
     "http://api.marketstack.com/v1/intraday?access_key=5be42204cd612e45b35d7b72aa26023f&symbols=AAPL&limit=20&offset=0&sort=DESC";
   const [data, isCompleted] = useFetch(Url);
+  console.log("Home, isComplete is : ",isCompleted);
   return isCompleted ? (
     function(){
       let TradeHistory = dataFormatter(data);
