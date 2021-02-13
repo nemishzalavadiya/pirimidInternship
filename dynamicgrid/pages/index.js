64e88f819234953c85fa47a1c57abb8f7c783fea
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-13 20:21:05 +0530

    added footer

--- dynamicgrid/pages/index.js
+++ dynamicgrid/pages/index.js
@@ -1,17 +1,23 @@
 import Loading from "./Loading";
 import Grid from "./Grid";
-import { useFetch } from './hook/useFetch';
+import { useFetch } from "./hook/useFetch";
 import dataFormatter from "./util/dataFormatter";
 export default function Home() {
   const Url =
     "http://api.marketstack.com/v1/intraday?access_key=5be42204cd612e45b35d7b72aa26023f&symbols=AAPL&limit=20&offset=0&sort=DESC";
   const [data, isCompleted] = useFetch(Url);
-  console.log("Home, isComplete is : ",isCompleted);
+  console.log("Home, isComplete is : ", isCompleted);
   return isCompleted ? (
-    function(){
+    (function () {
       let TradeHistory = dataFormatter(data);
-      return <Grid header={TradeHistory.header} data={TradeHistory.data} />
-    }()
+      return (
+        <>
+          {" "}
+          <Grid header={TradeHistory.header} data={TradeHistory.data} />
+          <h2 style={{textAlign:"center",paddingTop:'5px',borderTop:'1px solid black'}}> Total Entries: {TradeHistory.data.length}</h2>
+        </>
+      );
+    })()
   ) : (
     <Loading />
   );
