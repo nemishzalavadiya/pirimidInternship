5fe42fff4d780da5aaf856749fd7fb5748386fcb
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-15 20:59:29 +0530

    grid created

---
+++ semanticuigrid/pages/grid/GridContainer.js
@@ -0,0 +1,28 @@
+import { Grid} from "semantic-ui-react";
+export default function GridContainer(props) {
+
+    function conditionalDisplay(item){
+        if(Number.isInteger(item)){
+            return <i className="rupee sign icon small"></i>;
+        }
+    }
+
+  return (
+      <>
+    <div className="ui equal width grid" style={{margin:'100px 200px 100px 200px'}}>
+    <div className="ui horizontal divider" style={{color:'#fff'}}>{props.title}</div>
+      {props.data.map((row) => {
+        return (
+          <div className="ui row segment" style={{height:'20vh',padding:'5vh 2vh 5vh 2vh',backgroundColor:'rgb(33, 33, 33)',color:'#fff'}}>
+            {row.map((item,index) => (
+                index!=0?           
+                 <div className="column"><div style={{fontSize:'x-small'}}>{props.headerNames[index]}</div>{item}{" "}{conditionalDisplay(item)}  </div>
+                :<div className={`six wide column`}>{item}</div>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+    </>
+  );
+}
--- semanticuigrid/pages/index.js
+++ semanticuigrid/pages/index.js
@@ -1,65 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
+import GridContainer from "./grid/GridContainer";
 export default function Home() {
+  // Watchlist
+  let HeaderName = ["", "", "Open", "Close", "market"];
+  let Data = [
+    [
+      "ICICI Prudential Bluechip Fund Direct Plan Growth",
+      "Stock",
+      166,
+      189,
+      200,
+    ],
+    ["HDFC Corporation Fund Planing", "Stock", 166, 189, 200],
+    ["State Bank Of India Mutual Fund", "Stock", 166, 189, 200],
+    ["Municipal Corporation", "Stock", 166, 189, 200],
+    ["TCS", "Stock", 166, 189, 200],
+    ["Infosys", "Stock", 166, 189, 200],
+    ["Tata", "Stock", 166, 189, 200],
+    ["Google", "Stock", 166, 189, 200],
+    ["Tesla", "Stock", 166, 189, 200],
+    ["Oracle", "Stock", 166, 189, 200],
+    ["Microsoft", "Stock", 166, 189, 200],
+  ];
+  // Position
+  let HeaderNamePosition = ["", "", "Price", "Volume"];
+  let DataPosition = [
+    [
+      "ICICI Prudential Bluechip Fund Direct Plan Growth",
+      "Stock",
+      166,
+      "189,200",
+    ],
+    ["HDFC Corporation Fund Planing", "Stock", 166, "189,200"],
+    ["State Bank Of India Mutual Fund", "Stock", 166, "189,200"],
+    ["Municipal Corporation", "Stock", 166, "189,200"],
+    ["TCS", "Stock", 166, "189,200"],
+    ["Infosys", "Stock", 166, "189,200"],
+    ["Tata", "Stock", 166, "109,090"],
+    ["Google", "Stock", 166, "109,090"],
+    ["Tesla", "Stock", 166, "109,090"],
+    ["Oracle", "Stock", 166, "109,090"],
+    ["Microsoft", "Stock", 166, "109,090"],
+  ];
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <>
+      <div style={{ backgroundColor: "black" }}>
+        <GridContainer data={Data} headerNames={HeaderName} title={'Watchlist'}/>
+        <GridContainer data={DataPosition} headerNames={HeaderNamePosition} title={'Position'} />
+      </div>
+    </>
+  );
 }
